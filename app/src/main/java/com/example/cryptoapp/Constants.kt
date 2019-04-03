aa8ab21bdb16288bf45ba55d96b789c97005211a
Miguel Najera <migueln1987@gmail.com>
2019-04-02 20:50:52 -0400

    Add constants for Coin Market Cap API

--- app/src/main/java/com/example/cryptoapp/Constants.kt
+++ app/src/main/java/com/example/cryptoapp/Constants.kt
@@ -3,7 +3,9 @@ package com.example.cryptoapp
 object Constants {
 
     const val API_KEY = BuildConfig.ApiKey
+    const val MC_API_KEY = BuildConfig.MarketCapApiKey
     const val WS_COIN_URL = "wss://ws.coinapi.io/v1/"
+    const val MC_BASE_URL = "sandbox-api.coinmarketcap.com"
     const val HELLO_MESSAGE = "{\n" +
             "  \"type\": \"hello\",\n" +
             "  \"apikey\": \"$API_KEY\",\n" +
@@ -16,4 +18,9 @@ object Constants {
             "    \"ITBIT_\"\n" +
             "    ]\n" +
             "}"
+
+    const val CRYPTO_ENDPOINT = "/cryptocurrency/"
+    const val EXCHANGE_ENDPOINT = "/exchange/"
+    const val METRICS_ENDPOINT = "/global-metrics/"
+    const val TOOLS_ENDPOINT = "/tools/"
 }
--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_main_activity.text = Constants.MC_API_KEY
         }
     }
 
